00a204b07428c5cb80c17d33390d49540723e6ca
murrx <altenarobin@gmail.com>
2014-07-02 16:22:49 +0200

    first implementation of registration

--- dotNetEIndopdracht/SuperStoreLibrary/Communication/SuperStoreServiceImplementation.cs
+++ dotNetEIndopdracht/SuperStoreLibrary/Communication/SuperStoreServiceImplementation.cs
@@ -4,15 +4,40 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using SuperStoreLibrary.Model;
 
 namespace SuperStoreLibrary.Communication
 {
     public class SuperStoreServiceImplementation : SuperStoreServiceInterface
     {
 
-        public AuthenticationCredentials RegisterNewUser(CustomerContainer NewUser)
+        public AuthenticationCredentials RegisterNewUser(CustomerContainer RegisterCustomer)
         {
+            SuperStoreModelContainer ModelContainer = ModelContainerProvider.GetInstance();
+
+            //Check if username is still available
+            Customer ExistingCustomer = ModelContainer.Customers.FirstOrDefault(user => user.Username == RegisterCustomer.credentials.username);
+            if (ExistingCustomer == null){
+                Customer NewCustomer = new Customer{Name = RegisterCustomer.name, Username = RegisterCustomer.credentials.username, Password = GeneratePassword(RegisterCustomer.credentials.password)};
+                //add something to generate&encrypt password
+
+                ModelContainer.Customers.Add(NewCustomer);
+                ModelContainer.SaveChanges();
+
+                return RegisterCustomer.credentials;
+            }
             throw new FaultException("not yet implemented");
+        }
+
+        private string GeneratePassword(String Username){
+            string Password = "";
+
+            foreach(char Char in Username){
+                int C = Char;
+                Password = C++.ToString();
+            }
+
+            return Password;
         }
 
         public CustomerContainer RetrieveUserInfo(AuthenticationCredentials Credentials)
